9ec9054d947228dd97f8ecf9723a2d65a897703e
pras-4795 <pprasanth19954@gmail.com>
2023-10-17 00:21:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ model = load_model('iris_nn_model.h5')
 def predict(model, scaler, encoder, features):
     # Scale the features
     scaled_features = scaler.transform(features)
+    scaled_features = encoder.transform(features)
+    scaled_features = pd.get_dummies(scaled_features)
 
     # Predict using the Neural Network model
     prediction = model.predict(scaled_features)
@@ -24,21 +26,12 @@ def predict(model, scaler, encoder, features):
     return np.argmax(prediction, axis=1)
 
 # Title of the app
-st.title("Iris Flower Classifier")
+st.title("Home Credit Default Risk")
 
 # Instructions
 st.write("""
-Select the features of the Iris flower you'd like to classify:
+Upload the csv file of record you want to classify:
 """)
-
-# Sliders to input features
-sepal_length = st.slider("Sepal Length (cm)", 4.3, 7.9, 5.8)
-sepal_width = st.slider("Sepal Width (cm)", 2.0, 4.4, 3.1)
-petal_length = st.slider("Petal Length (cm)", 1.0, 6.9, 4.7)
-petal_width = st.slider("Petal Width (cm)", 0.1, 2.5, 1.4)
-
-# Arrange the features as an array
-features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
 
 # Title and instructions
 st.title("CSV File Upload and Prediction Display")
@@ -47,6 +40,7 @@ st.write("Upload a CSV file, and we'll display if there is a chance of default."
 # File uploader widget
 uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 
+df = pd.DataFrame()
 if uploaded_file is not None:
     # Check if a file has been uploaded
     df = pd.read_csv(uploaded_file)  # Read the uploaded CSV file into a DataFrame
@@ -58,8 +52,8 @@ if uploaded_file is not None:
     # You can perform further data analysis or visualization on the DataFrame here
 
 # Get the prediction
-prediction = predict(model, scaler, encoder, features)
+prediction = predict(model, scaler, encoder, df)
 
 # Display the prediction
 st.subheader("Prediction:")
-st.write(['setosa', 'versicolor', 'virginica'][prediction[0]])
+st.write(['Default', 'Does Not Default'][prediction[0]])
